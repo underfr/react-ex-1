40ea268fe223a432ef9c51558a9f785d95361ceb
underfr <loicklaurent28@gmail.com>
2026-05-05 16:45:20 +0200

    Feature: add condition for todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,47 @@ const TODOS = [
   {
     id: 0,
     todo:'Faire la vaiselle',
-    date:'20-01-2026'
+    date:'20-01-2026',
+    checked: true
   },
   {
     id: 1,
     todo:'Faire le ménage',
-    date: '21-03-2026'
+    date: '21-03-2026',
+    checked: false
   },
   {
     id: 2,
     todo:"Mettre de l'essence",
-    date:'30-03-2026'
+    date:'30-03-2026',
+    checked: true
   }
 ]
 
-function ToDo({todo, date}) {
+function ToDo({todo}) {
+    if(todo.checked) {
+      return (
+        <li className='green'><input type='checkbox' defaultChecked/>{todo.todo} - {todo.date}</li> 
+      )
+    } 
+    return (
+      <li className='orange'>{todo.todo} - {todo.date}</li>
+    )
+}
+
+function ToDoTernaire({todo}) {
   return (
-    <>
-      <li>{todo} - {date}</li>
-    </>
+    todo.checked
+    ? <li className='green'><input type='checkbox' defaultChecked/>{todo.todo} - {todo.date}</li> 
+    : <li className='orange'>{todo.todo} - {todo.date}</li> 
   )
+}
+
+function ToDoAnd({todo}){
+  return <> 
+    {todo.checked ? <li className='green'>{todo.todo} - {todo.date}</li> : <li className='orange'>{todo.todo} - {todo.date}</li>}
+    {todo.checked && <input type='checkbox' defaultChecked/>} 
+  </>
 }
 
 function App(){
@@ -33,8 +54,17 @@ function App(){
     <h1>Todo List</h1>
     <h2>{DATE.toLocaleString()}</h2>
     <ul>
+      <h2>ToDo IF</h2>
       {TODOS.map((todo)=>
-        <ToDo key={todo.id} todo={todo.todo} date={todo.date}/>
+        <ToDo key={todo.id} todo={todo}/>
+      )}
+      <h2>ToDoTernaire</h2>
+      {TODOS.map((todo)=>
+        <ToDoTernaire key={todo.id} todo={todo}/>
+      )}
+      <h2>ToDoAnd</h2>
+      {TODOS.map((todo)=>
+        <ToDoAnd key={todo.id} todo={todo}/>
       )}
     </ul>
     </>
