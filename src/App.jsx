39e9433408bee84bdf38a0384120633dcf6d2b13
underfr <loicklaurent28@gmail.com>
2026-05-05 09:42:38 +0200

    Feature: Map on ToDo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
 
+const TODOS = [
+  {
+    id: 0,
+    todo:'Faire la vaiselle',
+    date:'20-01-2026'
+  },
+  {
+    id: 1,
+    todo:'Faire le ménage',
+    date: '21-03-2026'
+  },
+  {
+    id: 2,
+    todo:"Mettre de l'essence",
+    date:'30-03-2026'
+  }
+]
+
 function ToDo({todo, date}) {
   return (
     <>
-    <ul>
       <li>{todo} - {date}</li>
-    </ul>
     </>
   )
 }
@@ -17,9 +33,9 @@ function App(){
     <h1>Todo List</h1>
     <h2>{DATE.toLocaleString()}</h2>
     <ul>
-      <ToDo todo={'Faire la vaiselle'} date={"20-01-2026"}/>
-      <ToDo todo={'Faire le ménage'} date={"21-03-2026"}/>
-      <ToDo todo={"Mettre de l'essence"} date={"30-03-2026"}/>
+      {TODOS.map((todo)=>
+        <ToDo key={todo.id} todo={todo.todo} date={todo.date}/>
+      )}
     </ul>
     </>
   )
